45d368cbe6493ecccf8247fd472bf1c0737d4776
Denver Coneybeare <dconeybe@google.com>
2022-06-20 16:24:01 -0400

    GROUP BY and other AggregateField types added

--- firebase-firestore/src/main/java/com/google/firebase/firestore/AggregateDemo.java
+++ firebase-firestore/src/main/java/com/google/firebase/firestore/AggregateDemo.java
@@ -14,7 +14,14 @@
 
 package com.google.firebase.firestore;
 
+import static com.google.firebase.firestore.AggregateField.count;
+
+import android.app.Activity;
 import androidx.annotation.NonNull;
+import java.util.concurrent.Executors;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 final class AggregateDemo {
 
@@ -27,14 +34,64 @@ final class AggregateDemo {
   void countBasicQuery() {
     AggregateQuery query = db.collection("users").count();
     AggregateQuerySnapshot snapshot = query.get(AggregateSource.SERVER_DIRECT).getResult();
-    assertEqual(snapshot.getCount(), 50);
+    assertEqual(snapshot.get(count()), 50);
   }
 
   void countLimitNumRowsScanned() {
     AggregateQuery query = db.collection("users").limit(11).count();
     AggregateQuerySnapshot snapshot = query.get(AggregateSource.SERVER_DIRECT).getResult();
-    assertEqual(snapshot.getCount(), 11);
+    assertEqual(snapshot.get(count()), 11);
+  }
+
+  void countUpTo() {
+    AggregateQuery query = db.collection("users").aggregate(count().upTo(11));
+    AggregateQuerySnapshot snapshot = query.get(AggregateSource.SERVER_DIRECT).getResult();
+    assertEqual(snapshot.get(count()), 11);
+  }
+
+  void countRealTimeUpdates() throws InterruptedException {
+    AggregateQuery query = db.collection("users").aggregate(count());
+
+    Lock lock = new ReentrantLock();
+    Condition condition = lock.newCondition();
+
+    ListenerRegistration registration =
+        query
+            .listen()
+            .startDirectFromServer(
+                (snapshot, error) -> {
+                  assertNull(error);
+                  assertNotNull(snapshot);
+                  assertEqual(snapshot.get(count()), 50);
+                  assertFalse(snapshot.getMetadata().isFromCache());
+                  assertFalse(snapshot.getMetadata().hasPendingWrites());
+                  lock.lock();
+                  try {
+                    condition.signalAll();
+                  } finally {
+                    lock.unlock();
+                  }
+                });
+
+    lock.lock();
+    try {
+      condition.await();
+    } finally {
+      lock.unlock();
+    }
+
+    registration.remove();
   }
 
   private static void assertEqual(Object o1, Object o2) {}
+
+  private static void assertNull(Object o) {}
+
+  private static void assertFalse(Object o) {}
+
+  private static void assertNotNull(Object o) {
+    if (o == null) {
+      throw new NullPointerException();
+    }
+  }
 }
